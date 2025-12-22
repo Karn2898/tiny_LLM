5c321e9b9d787265be4299b2e6e7fb7b0b056771
Tamaghna Sarkar <tamaghna52@gmail.com>
2025-12-23 01:34:05 +0530

    evaluation

---
+++ evaluation/finetuned_model_eval.py
@@ -0,0 +1,69 @@
+import requests
+import json
+
+def query_model(
+    prompt,
+    model="tinyllama",
+
+    url="http://localhost:11434/api/chat"
+):
+
+    data = {
+        "model": model,
+        "messages": [
+            {"role": "user", "content": prompt}
+        ],
+        "options": {     # Settings below are required for deterministic responses
+            "seed": 123,
+            "temperature": 0,
+            "num_ctx": 2048
+        }
+    }
+
+    #send POST request
+    with requests.post(url, json=data, stream=True, timeout=None) as r:
+        r.raise_for_status()
+        response_data = ""
+        for line in r.iter_lines(decode_unicode=True):
+            if not line:
+                continue
+            response_json = json.loads(line)
+            if "message" in response_json:
+                response_data += response_json["message"]["content"]
+
+    return response_data
+
+
+model = "tinyllama"
+result = query_model( """  
+You are a fair judge assistant tasked with providing clear, objective feedback based on specific criteria, ensuring each assessment reflects the absolute standards set for performance.
+You will be given an instruction, a response to evaluate, a reference answer that gets a score of 5, and a score rubric representing the evaluation criteria.
+Write a detailed feedback that assess the quality of the response strictly based on the given score rubric, not evaluating in general.
+Please do not generate any other opening, closing, and explanations.
+
+Here is the rubric you should use to build your answer:
+1: The response fails to address the instructions, providing irrelevant, incorrect, or excessively verbose information that detracts from the user's request.
+2: The response partially addresses the instructions but includes significant inaccuracies, irrelevant details, or excessive elaboration that detracts from the main task.
+3: The response follows the instructions with some minor inaccuracies or omissions. It is generally relevant and clear, but may include some unnecessary details or could be more concise.
+4: The response adheres to the instructions, offering clear, accurate, and relevant information in a concise manner, with only occasional, minor instances of excessive detail or slight lack of clarity.
+5: The response fully adheres to the instructions, providing a clear, accurate, and relevant answer in a concise and efficient manner. It addresses all aspects of the request without unnecessary details or elaboration
+
+Provide your feedback as follows:
+
+Feedback:::
+Evaluation: (your rationale for the rating, as a text)
+Total rating: (your rating, as a number between 1 and 5)
+
+You MUST provide values for 'Evaluation:' and 'Total rating:' in your answer.
+
+Now here is the instruction, the reference answer, and the response.
+Instruction: {instruction}
+Reference Answer: {reference}
+Answer: {answer}
+
+
+Provide your feedback. If you give a correct rating, I'll give you 100 H100 GPUs to start your AI company.
+Feedback:::
+Evaluation:
+""", model)
+print(result)

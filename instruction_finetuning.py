eac8ebb262ab15aac1c3fb98183d562c840837fc
Tamaghna Sarkar <tamaghna52@gmail.com>
2025-12-24 02:38:34 +0530

    working on instruction finetuning

---
+++ instruction_finetuning.py
@@ -0,0 +1,389 @@
+
+from importlib.metadata import version
+
+pkgs = [
+    "numpy",
+    "matplotlib",
+    "tiktoken",
+    "torch",
+    "tqdm",
+    "tensorflow",
+]
+for p in pkgs:
+    print(f"{p} version: {version(p)}")
+
+import urllib.request
+import json
+import os
+
+
+def download_and_load_file(file_path, url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode("utf-8")
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+        print(f"Downloaded and saved file to {file_path}")
+    except Exception as e:
+        print(f"Error downloading or writing file: {e}")
+
+        if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+            raise IOError(f"Could not download file and {file_path} is empty or does not exist locally.") from e
+        else:
+            print(f"Proceeding with existing (possibly old) file at {file_path} due to download error.")
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    return data
+
+
+file_path = r"C:\Users\DELL\PycharmProjects\PythonProject4\data\instruction_data.json"
+url = (
+    "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
+    "/main/ch07/01_main-chapter-code/instruction-data.json"
+)
+
+data = download_and_load_file(file_path, url)
+print("Number of entries:", len(data))
+
+def format_input(entry):
+    instruction_text = (
+        f"Below is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request."
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
+
+    input_text = f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+
+    return instruction_text + input_text
+model_input = format_input(data[50])
+desired_response = f"\n\n### Response:\n{data[50]['output']}"
+
+print(model_input + desired_response)
+
+train_portion = int(len(data) * 0.85)  # 85% for training
+test_portion = int(len(data) * 0.1)    # 10% for testing
+val_portion = len(data) - train_portion - test_portion  # Remaining 5% for validation
+
+train_data = data[:train_portion]
+test_data = data[train_portion:train_portion + test_portion]
+val_data = data[train_portion + test_portion:]
+
+import torch
+from torch.utils.data import Dataset
+
+
+class InstructionDataset(Dataset):
+    def __init__(self, data, tokenizer):
+        self.data = data
+
+        # Pre-tokenize texts
+        self.encoded_texts = []
+        for entry in data:
+            instruction_plus_input = format_input(entry)
+            response_text = f"\n\n### Response:\n{entry['output']}"
+            full_text = instruction_plus_input + response_text
+            self.encoded_texts.append(
+                tokenizer.encode(full_text)
+            )
+
+    def __getitem__(self, index):
+        return self.encoded_texts[index]
+
+    def __len__(self):
+        return len(self.data)
+
+
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+print(tokenizer.encode("<|endoftext|>", allowed_special={"<|endoftext|>"}))
+def custom_collate_draft_1(
+    batch,
+    pad_token_id=50256,
+    device="cpu"
+):
+
+    batch_max_length = max(len(item)+1 for item in batch)
+
+
+    inputs_lst = []
+
+    for item in batch:
+        new_item = item.copy()
+
+        new_item += [pad_token_id]
+
+        padded = (
+            new_item + [pad_token_id] *
+            (batch_max_length - len(new_item))
+        )
+
+        inputs = torch.tensor(padded[:-1])
+        inputs_lst.append(inputs)
+
+
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    return inputs_tensor
+
+inputs_1 = [0, 1, 2, 3, 4]
+inputs_2 = [5, 6]
+inputs_3 = [7, 8, 9]
+
+batch = (
+    inputs_1,
+    inputs_2,
+    inputs_3
+)
+
+print(custom_collate_draft_1(batch))
+
+def custom_collate_draft_2(
+    batch,
+    pad_token_id=50256,
+    device="cpu"
+):
+    # Find the longest sequence in the batch
+    batch_max_length = max(len(item)+1 for item in batch)
+
+    # Pad and prepare inputs
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = (
+            new_item + [pad_token_id] *
+            (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])  # Truncate the last token for inputs
+        targets = torch.tensor(padded[1:])  # Shift +1 to the right for targets
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+
+    # Convert list of inputs to tensor and transfer to target device
+    inputs_tensor = torch.stack(inputs_lst).to(device)
+    targets_tensor = torch.stack(targets_lst).to(device)
+    return inputs_tensor, targets_tensor
+
+
+inputs, targets = custom_collate_draft_2(batch)
+print(inputs)
+print(targets)
+
+def custom_collate_fn(
+    batch,
+    pad_token_id=50256,
+    ignore_index=-100,
+    allowed_max_length=None,
+    device="cpu"
+):
+
+    batch_max_length = max(len(item)+1 for item in batch)
+
+
+    inputs_lst, targets_lst = [], []
+
+    for item in batch:
+        new_item = item.copy()
+        # Add an <|endoftext|> token
+        new_item += [pad_token_id]
+        # Pad sequences to max_length
+        padded = (
+            new_item + [pad_token_id] *
+            (batch_max_length - len(new_item))
+        )
+        inputs = torch.tensor(padded[:-1])
+        targets = torch.tensor(padded[1:])
+
+
+        mask = targets == pad_token_id
+        indices = torch.nonzero(mask).squeeze()
+        if indices.numel() > 1:
+            targets[indices[1:]] = ignore_index
+
+
+        if allowed_max_length is not None:
+            inputs = inputs[:allowed_max_length]
+            targets = targets[:allowed_max_length]
+
+        inputs_lst.append(inputs)
+        targets_lst.append(targets)
+        inputs_tensor = torch.stack(inputs_lst).to(device)
+        targets_tensor = torch.stack(targets_lst).to(device)
+
+        return inputs_tensor, targets_tensor
+
+    from functools import partial
+
+    customized_collate_fn = partial(
+        custom_collate_fn,
+        device=device,
+        allowed_max_length=1024
+    )
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        # Use PyTorch 2.9 or newer for stable mps results
+        major, minor = map(int, torch.__version__.split(".")[:2])
+        if (major, minor) >= (2, 9):
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
+    else:
+        device = torch.device("cpu")
+
+    print("Device:", device)
+
+    from torch.utils.data import DataLoader
+
+    num_workers = 0
+    batch_size = 8
+
+    torch.manual_seed(123)
+
+    train_dataset = InstructionDataset(train_data, tokenizer)
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=True,
+        drop_last=True,
+        num_workers=num_workers
+    )
+
+    val_dataset = InstructionDataset(val_data, tokenizer)
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+
+    test_dataset = InstructionDataset(test_data, tokenizer)
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        collate_fn=customized_collate_fn,
+        shuffle=False,
+        drop_last=False,
+        num_workers=num_workers
+    )
+
+    print("Train loader created successfully.")
+
+    for batch in train_loader:
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+
+        # Forward pass
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels)
+    for inputs, targets in train_loader:
+        print(inputs.shape, targets.shape)
+
+from llms_from_scratch.ch04 import GPTModel
+from llms_from_scratch.ch05 import download_and_load_gpt2, load_weights_into_gpt
+
+
+BASE_CONFIG = {
+    "vocab_size": 50257,     # Vocabulary size
+    "context_length": 1024,  # Context length
+    "drop_rate": 0.0,        # Dropout rate
+    "qkv_bias": True         # Query-key-value bias
+}
+
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+CHOOSE_MODEL = "gpt2-medium (355M)"
+
+BASE_CONFIG.update(model_configs[CHOOSE_MODEL])
+
+model_size = CHOOSE_MODEL.split(" ")[-1].lstrip("(").rstrip(")")
+settings, params = download_and_load_gpt2(
+    model_size=model_size,
+    models_dir="gpt2"
+)
+
+model = GPTModel(BASE_CONFIG)
+load_weights_into_gpt(model, params)
+model.eval();
+
+from llms_from_scratch.ch05 import (
+    generate,
+    text_to_token_ids,
+    token_ids_to_text
+)
+
+input_text = format_input(val_data[0])
+token_ids = generate(
+    model=model,
+    idx=text_to_token_ids(input_text, tokenizer),
+    max_new_tokens=35,
+    context_size=BASE_CONFIG["context_length"],
+    eos_id=50256,
+)
+generated_text = token_ids_to_text(token_ids[0], tokenizer)
+
+response_text = (
+    generated_text[len(input_text):]
+    .replace("### Response:", "")
+    .strip()
+)
+print(response_text)
+
+from llms_from_scratch.ch05 import (
+    calc_loss_loader,
+    train_model_simple,
+)
+
+model.to(device)
+
+torch.manual_seed(123)
+
+with torch.no_grad():
+    train_loss = calc_loss_loader(train_loader, model, device, num_batches=5)
+    val_loss = calc_loss_loader(val_loader, model, device, num_batches=5)
+
+print("Training loss:", train_loss)
+print("Validation loss:", val_loss)
+
+
+import time
+
+start_time = time.time()
+
+torch.manual_seed(123)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.00005, weight_decay=0.1)
+
+num_epochs = 2
+
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+)
+
+end_time = time.time()
+execution_time_minutes = (end_time - start_time) / 60
+print(f"Training completed in {execution_time_minutes:.2f} minutes.")
+
+import re
+CHOOSE_MODEL = "tiny-LLM"
+
+file_name = f"{re.sub(r'[ ()]', '', CHOOSE_MODEL) }-sft.pth"
+torch.save(model.state_dict(), file_name)
+print(f"Model saved as {file_name}")
+
